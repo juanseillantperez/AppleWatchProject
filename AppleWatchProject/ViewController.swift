2f11c2217553b2aa08e1ccc38fe968933655b726
Juan Seillant <jesp.90lp@gmail.com>
2021-02-05 13:34:48 -0300

    Initial Commit

---
+++ AppleWatchProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleWatchProject
+//
+//  Created by Juan Enrique Seillant Perez on 05/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
